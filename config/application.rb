13d5154ada809f21d53699d0bace4892b1a0adb3
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-14 23:52:12 +0800

    fix: configure middlewares for handling session bug

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,10 @@ module FinalProjBe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # FOR HANDLING SESSION
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
