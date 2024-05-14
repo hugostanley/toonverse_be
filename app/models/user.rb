0947e3a9e5134679bb0a05d96aaf6fde52127978
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-14 23:01:43 +0800

    feat: create user model and config auth

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
