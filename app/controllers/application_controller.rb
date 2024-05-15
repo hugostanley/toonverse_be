d7cdab864fdaed8a444a95b73c12f2d74d7ad8f6
Louise Dungca <dungca.louisechristine@gmail.com>
2024-05-15 11:57:53 +0800

    chore: fix indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        # before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-        protected
+  protected
 
-        # def configure_permitted_parameters
-        # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        # end
+  # def configure_permitted_parameters
+  # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  # end
 end
