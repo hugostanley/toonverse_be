7999bb70c5a4990dd140c4da84d464e0e2e06266
cyrilcero <cyril.cero@gmail.com>
2024-05-16 21:57:42 +0800

    fix: add scope to google omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, Rails.application.credentials.oauth.GOOGLE_CLIENT_ID, Rails.application.credentials.oauth.GOOGLE_CLIENT_SECRET,
+  {
+    scope: 'email, profile', # This will allow us to get the user's email address and profile picture.
+    prompt: 'select_account', # This will allow the user to select which account they want to login with.
+    image_aspect_ratio: 'square', # This will make sure that the profile picture is a square.
+    image_size: 50, # This will make sure that the profile picture is 50x50 pixels.
+  }
+end
