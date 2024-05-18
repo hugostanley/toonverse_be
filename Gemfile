c2cfaf96bbd2ab3b9e54e95c952ea36a8f609975
jeanbejar <jeanevabejar@gmail.com>
2024-05-18 09:39:40 +0800

    Revert "Merge branch 'feat/invitable' into chore/table"
    This reverts commit 01eb6a2ffa9f01c0b0ba93b2455a15f66eac90d1, reversing
    changes made to dbc554154b952f91d00bbfcd7d4c72c04e4bb737.

--- Gemfile
+++ Gemfile
@@ -49,7 +49,3 @@ end
 gem 'devise_token_auth', git: 'https://github.com/lynndylanhurley/devise_token_auth'
 
 gem "devise", "~> 4.9"
-
-gem "devise_invitable", "~> 2.0"
-
-gem "ruby-lsp", "~> 0.16.6"
--- db/schema.rb
+++ db/schema.rb
@@ -144,19 +144,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_17_170434) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.bigint "invited_by_id"
-    t.integer "invitations_count", default: 0
     t.index ["confirmation_token"], name: "index_workforces_on_confirmation_token", unique: true
     t.index ["email"], name: "index_workforces_on_email", unique: true
-    t.index ["invitation_token"], name: "index_workforces_on_invitation_token", unique: true
-    t.index ["invited_by_id"], name: "index_workforces_on_invited_by_id"
-    t.index ["invited_by_type", "invited_by_id"], name: "index_workforces_on_invited_by"
     t.index ["reset_password_token"], name: "index_workforces_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_workforces_on_uid_and_provider", unique: true
   end
