9bb21d064c8f95d2fa1bf58944f3462788a35da7
jeanbejar <jeanevabejar@gmail.com>
2024-05-19 00:49:44 +0800

    refactor: change origin in 'cor' to an array

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins ['http://localhost:5173', 'http://127.0.0.1:5173']
 
     resource "*",
       headers: :any,
