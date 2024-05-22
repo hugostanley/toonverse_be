a61f778934502c70c5331a17360e92b1f96971e5
jewlmils <st.jewelmilagrosa@gmail.com>
2024-05-22 22:55:38 +0800

    docs: restore schema information comment in Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id              :bigint           not null, primary key
+#  amount          :decimal(15, 2)
+#  art_style       :enum             not null
+#  background_url  :string           not null
+#  notes           :string
+#  number_of_heads :integer          default(0), not null
+#  picture_style   :enum             not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  payment_id      :bigint
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_items_on_payment_id  (payment_id)
+#  index_items_on_user_id     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :payment, optional: true
