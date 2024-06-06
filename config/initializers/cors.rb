179df062cb256453147023bc1a9e349a5a6c9f0f
cyrilcero <cyril.cero@gmail.com>
2024-06-06 17:46:25 +0800

    fix: add additional origin for deployment testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://toonverse-fe.vercel.app'
+    origins 'http://localhost:5173', 'https://toonverse-fe.vercel.app', 'https://toonverse-eta.vercel.app/'
 
     resource '*',
              headers: :any,
